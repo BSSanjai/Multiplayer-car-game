bf70f91d23d39b4467853f9829a578ae24ee762a
BSSanjai <69799493+BSSanjai@users.noreply.github.com>
2020-10-09 17:31:44 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,94 @@
+class Game {
+  constructor(){
+this.carImage1 = loadImage("images/car1.png");
+this.carImage2 = loadImage("images/car2.png");
+this.carImage3 = loadImage("images/car3.png");
+this.carImage4 = loadImage("images/car4.png");
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }else{
+        playerCount =0;
+        player.updateCount(0);
+      }
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(100,200);
+    car1.addImage(this.carImage1);
+    car2 = createSprite(300,200);
+    car2.addImage(this.carImage2);
+    car3 = createSprite(500,200);
+    car3.addImage(this.carImage3);
+    car4 = createSprite(700,200);
+    car4.addImage(this.carImage4);
+    cars = [car1, car2, car3, car4];
+  }
+
+  play(){
+    form.hide();
+
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      //var display_position = 100;
+      
+      //index of the array
+      var index = 0;
+
+      //x and y position of the cars
+      var x = 0;
+      var y;
+
+      for(var plr in allPlayers){
+        //add 1 to the index for every loop
+        index = index + 1 ;
+
+        //position the cars a little away from each other in x direction
+        x = x + 200;
+        //use data form the database to display the cars in y direction
+        y = displayHeight - allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+
+        if (index === player.index){
+          cars[index - 1].shapeColor = "red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y
+        }
+       
+        //textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+
+    drawSprites();
+  }
+}
